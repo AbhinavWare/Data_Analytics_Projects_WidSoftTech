5aa11ad6a55186a1137bcb056dbe6964cf498123
Abhinav .K <askygpn@gmail.com>
2026-06-10 16:06:19 +0530

    Added Basics python file with base code.
    This is first basic code update.

---
+++ Basics.py
@@ -0,0 +1,33 @@
+#Starts with variables used to store data or values which are used in operations and can be reused in programs.
+name=input("Enter your name:")
+
+#name is variable "Abhinav" is string value stored in string variable. 
+#Python automatically detects data type of variable based on value stored. 
+print(name)
+#prints parameter provided in () onto console screen.
+
+print(type(name))
+#type() function prints or returns the data type class of variable belonging to.
+
+marks=10
+print(marks) 
+print(type(marks))
+
+marks=input("Enter your marks:")
+print(marks)
+print(type(marks))
+
+a=0
+isZero=False
+if a==1:
+    isZero = True
+else:
+    print(isZero)
+print(isZero)
+
+
+#All the above code was written on 09.06.2026.
+
+print("Java","C++","C",sep='-') #Can use -,_ ,etc.
+#sep stands for seprator which seprates the , text in print function using the symbol or data assigned to sep= and seprates text. 
+
